0ff4e4ffa02c200e54846774f9e66fa010a1636e
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-04-12 17:54:23 +0200

    feat: feat found solution for task 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,13 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  // throw new Error('Not implemented');
+  let arr = email.split('@').reverse();
+
+  // console.log(arr[0]);
+
+  return arr[0];
 }
 
 module.exports = getEmailDomain;
